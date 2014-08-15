e87580c1e32ff93281a5b151d53213593b2b9b51
Martin Heidegger <martin.heidegger@gmail.com>
2014-08-16 02:00:59 +0900

    Using different setup

--- Berksfile
+++ Berksfile
@@ -1,3 +1,3 @@
-site :opscode
-
-metadata
+source "https://api.berkshelf.com" 
+ 
+cookbook "newrelic", git: 'git://github.com/escapestudios-cookbooks/newrelic.git', tag: '1.0.6'
--- newrelic/attributes/repository.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: newrelic
-# Attributes:: repository
-#
-# Copyright 2012-2014, Escape Studios
-#
-
-default['newrelic']['repository']['repository_key'] = '548C16BF'
-default['newrelic']['repository']['repository_action'] = :install
